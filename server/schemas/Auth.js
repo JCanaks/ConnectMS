cebdaed7cadddf940dd242f772bfdff3a7af2658
JCanaks <chiomimo15@gmail.com>
2019-07-17 17:32:02 +0100

    ISSUE-#3(authentication): implement authentication for the system
    - setup the auth schema
    - setup signup authentication
    - setup login authetication
    - implement related resolvers
    
    [ISSUE #3]

---
+++ server/schemas/Auth.js
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
+
+const { APP_SECRET } = process.env;
+
+export const typeDef = `
+extend type Mutation {
+    signup(name: String!, phoneNumber: String!, password: String!): AuthPayload
+    login(phoneNumber: String!, password: String!): AuthPayload
+}
+
+type AuthPayload {
+    token: String
+    user: Contact
+}`;
+
+export const resolvers = {
+  Mutation: {
+    signup: async (parent, args, context, info) => {
+      const password = await bcrypt.hash(args.password, 10);
+      const user = await context.prisma.createContact({ ...args, password });
+      const token = jwt.sign({ userId: user.id }, APP_SECRET, { expiresIn: '3h' });
+
+      return {
+        token,
+        user,
+      };
+    },
+    login: async (parent, args, context, info) => {
+      const user = await context.prisma.contact({ phoneNumber: args.phoneNumber });
+
+      if (!user) {
+        throw new Error('No such user found');
+      }
+
+      const valid = await bcrypt.compare(args.password, user.password);
+      if (!valid) {
+        throw new Error('Invalid Password');
+      }
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET, { expiresIn: '3h' });
+
+      return {
+        token,
+        user,
+      };
+    },
+  },
+};
